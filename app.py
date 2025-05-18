7bb4ecf934fe778c60c56afbca058b6500b15b55
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-18 19:31:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 # Зареждаме модела (адаптирай името, ако е различно)
-model = tf.keras.models.load_model("converted_model.keras")
+model = tf.keras.models.load_model("converted_model.h5")
 
 st.title("Птици - Разпознаване на изображения 🐦")
 
@@ -14,16 +14,13 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Качено изображение", use_column_width=True)
 
-    # Примерна подготовка на изображението - настрой според входния размер на твоя модел
-    img_size = (224, 224)  # смени ако трябва
+    img_size = (224, 224)  # смени според твоя модел
     img = image.resize(img_size)
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)  # batch dimension
 
     if st.button("Разпознай"):
         prediction = model.predict(img_array)
-        # Тук трябва да обработиш output-a на модела според твоите класове
-        # Например, ако имаш класове в списък:
         classes = ["parrot", "white stork"]  # смени с твоите класове
         predicted_class = classes[np.argmax(prediction)]
 
